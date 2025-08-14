7732c277f1f8163a776d660f253233588d980f57
Gael Castro <gaelcastrolara2006@gmail.com>
2025-08-14 15:16:05 -0600

    a

--- Codigo/LinkedList.java
+++ Codigo/LinkedList.java
@@ -1,5 +1,17 @@
 package Actividad1.Actividad1.Codigo;
 
+import java.util.List;
+
 public class LinkedList<I> {
-    private Node<I> head;
+    private Node<I> cabeza;
+
+    public LinkedList() {
+        this.cabeza = null;
+    }
+    public LinkedList(Node<I> cabeza){
+        this.cabeza = cabeza;
+    }
+    public Node<I> getCabeza() {
+        return cabeza;
+    }
 }
--- Codigo/Node.java
+++ Codigo/Node.java
@@ -1,11 +1,14 @@
 package Actividad1.Actividad1.Codigo;
 
 public class Node<I> {
-    I data;
-    Node<I> next;
+    I datos;
+    Node<I> siguiente;
 
-    public Node(I data){
-        this.data = data;
-        this.next = null;
+    public Node(I datos, Node<I> siguiente) {
+        this.datos = datos;
+        this.siguiente = null;
+    }
+    public Node(I datos) {
+        this.datos = datos;
     }
 }
