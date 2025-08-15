e292ca9fdf89d00b115edbb65c1cf8a10959087f
Gael Castro <gaelcastrolara2006@gmail.com>
2025-08-15 09:45:58 -0600

    cambios

--- Codigo/LinkedList.java
+++ Codigo/LinkedList.java
@@ -4,22 +4,56 @@ import java.util.List;
 
 public class LinkedList<I> {
     private Node<I> cabeza;
+    private Node<I> cola;
 
     public LinkedList() {
         this.cabeza = null;
+        this.cola = null;
     }
-    public LinkedList(Node<I> cabeza){
+    public LinkedList(Node<I> cabeza, Node<I> cola) {
         this.cabeza = cabeza;
+        this.cola = cola;
     }
     public Node<I> getCabeza() {
         return cabeza;
     }
 
-    public void insertarCabeza(I datos) {
-        Node<I> nuevoNodo = new Node<>(datos, cabeza);
-        cabeza = nuevoNodo;
-        }
+    public Node<I> getCola() {
+        return cola;
+    }
 
+    public void insertarCabeza(I datos) {
+        Node<I> nuevoNodo = new Node<>(datos, cabeza, null);
+        if (cabeza == null) {
+            cabeza = nuevoNodo;
+            cola = nuevoNodo;
+        } else {
+            nuevoNodo.siguiente = cabeza;
+            cabeza.anterior = nuevoNodo;
+            cabeza = nuevoNodo;
+
+        }
+    }
+    
+    public void insertarCola(I datos) {
+        Node<I> nuevoNodo = new Node<>(datos);
+        if (cabeza == null) {
+            cabeza = nuevoNodo;
+            cola = nuevoNodo;
+        } else {
+            cola.siguiente = nuevoNodo;
+            nuevoNodo.anterior = cola;
+            cola = nuevoNodo;
+        }
         
     }
+
+    public void mostrarLista() {
+        Node<I> actual = cabeza;
+        while (actual != null) {
+            System.out.print(actual.datos + " ");
+            actual = actual.siguiente;
+        }
+    }
+}
 
--- Codigo/Node.java
+++ Codigo/Node.java
@@ -3,12 +3,15 @@ package Actividad1.Actividad1.Codigo;
 public class Node<I> {
     I datos;
     Node<I> siguiente;
+    Node<I> anterior;
 
-    public Node(I datos, Node<I> siguiente) {
+    public Node(I datos, Node<I> siguiente, Node<I> anterior) {
         this.datos = datos;
         this.siguiente = null;
+        this.anterior = null;
     }
     public Node(I datos) {
         this.datos = datos;
+        
     }
 }
