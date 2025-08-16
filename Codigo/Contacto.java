5b3d0ee17099688438dffe7a5619f9930951d424
Gael Castro <gaelcastrolara2006@gmail.com>
2025-08-16 16:11:33 -0600

    ola

---
+++ Codigo/Contacto.java
@@ -0,0 +1,31 @@
+package Actividad1.Actividad1.Codigo;
+
+public class Contacto {
+    private String nombre;
+    private int telefono;
+    private String direccion;
+
+    public Contacto(String nombre, int telefono, String direccion) {
+        this.nombre = nombre;
+        this.telefono = telefono;
+        this.direccion = direccion;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public int getTelefono() {
+        return telefono;
+    }
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+    public String getDireccion() {
+        return direccion;
+    }
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+}
